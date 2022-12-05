4dbd7ecbac3d8b1df02c1bc1ebe60427bbd0f510
jxes6102 <jxes6102@gmail.com>
2022-12-05 12:03:08 +0800

    temp12051200

--- src/app/common.const.ts
+++ src/app/common.const.ts
@@ -2,10 +2,14 @@ export const buttonTData = [
     {
         text:'對戰模式',
         mode:'battle',
+        className:'battleStyle',
+        isClose:false
     },
     {
         text:'紀錄模式',
         mode:'record',
+        className:'recordStyle',
+        isClose:true
     },
   ];
   
