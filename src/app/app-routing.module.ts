e618932f5f62d35e257d08d90f7e461dd8d143d5
jxes6102 <jxes6102@gmail.com>
2022-12-21 14:29:20 +0800

    QQQQQQQQQQQQQQQQQQQQ

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { AllViewComponent } from './all-view/all-view.component';
 import { ChoseComponent } from './chose/chose.component';
 import { GameViewComponent } from './game-view/game-view.component';
 
 const routes: Routes = [
   { path: '', component: ChoseComponent},
   { path: 'game', component: GameViewComponent},
+  { path: 'all', component: AllViewComponent},
   {
     path: '**',
     redirectTo: '',
