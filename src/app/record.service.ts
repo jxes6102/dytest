2f0b1b179cd1d962b2828b5944b4e4983b01683f
jxes6102 <jxes6102@gmail.com>
2023-01-09 13:36:46 +0800

    temp01090140

--- src/app/record.service.ts
+++ src/app/record.service.ts
@@ -9,47 +9,27 @@ export class RecordService {
     allRecords 所有遊戲紀錄
     checkRecord 紀錄每個格子的修改
   */
-  nowRecord:stepType[] = []
-  allRecords:stepType[][] = []
   checkRecord:stepType[][] = new checkData().getData
-
-
-
   testallRecords:stepType[][][] = []
   grabData:stepType[] = []
   testnowRecord:stepType[][] = []
 
-
-
-
   constructor() {}
   //拿取現在紀錄
   get getNowRecord() {
-    // return this.nowRecord
     return this.testnowRecord
   }
   //修改現在紀錄
   setNowRecord(val:number) {
-    if(val) this.nowRecord = this.allRecords[val]
-    else this.nowRecord = []
-
-
-
     if(val) this.testnowRecord = this.testallRecords[val]
     else this.testnowRecord = []
   }
   //拿取紀錄
   get getAllRecords() {
-    // return this.allRecords
     return this.testallRecords
   }
   //修改紀錄
   updatedAllRecords(index:number,data:stepType,status:string[]) {
-    if(!this.allRecords[index]?.length) this.allRecords[index] = []
-    this.allRecords[index].push(data)
-
-
-
     if(!this.testallRecords[index]?.length) this.testallRecords[index] = []
     // 當上一步是拿取時，將點擊和拿取組合成同一步驟
     if(this.grabData.length) {
@@ -65,10 +45,6 @@ export class RecordService {
 
   }
   clearAllRecords() {
-    this.allRecords[0] = []
-
-
-
     this.testallRecords[0] = []
   }
   //拿取格子紀錄
@@ -76,14 +52,8 @@ export class RecordService {
     return this.checkRecord
   }
   //修改格子紀錄
-  // QQQQ
-  // setCheckRecord(data:stepType[]) {
-  //   this.clearCheckRecord()
-  //   for(let item of data)this.updatedCheckRecord(item.wherePlace,{wherePlace: item.wherePlace,content: item.content,useSize:item.useSize,stepID:this.checkRecord[item.wherePlace].length,status:item.status})
-  // }
   setCheckRecord(data:stepType[][]) {
     this.clearCheckRecord()
-  
     for(let items of data) {
       for(let item of items) this.updatedCheckRecord(item.wherePlace,{wherePlace: item.wherePlace,content: item.content,useSize:item.useSize,stepID:this.checkRecord[item.wherePlace].length,status:item.status})
     }
@@ -97,45 +67,15 @@ export class RecordService {
   }
   //拿取本地紀錄
   setLocal() {
-    // if(localStorage.getItem('record')) this.allRecords = JSON.parse(localStorage.getItem('record') || '[]')
-    // else this.allRecords = []
-
-
-
     if(localStorage.getItem('record')) this.testallRecords = JSON.parse(localStorage.getItem('record') || '[]')
     else this.testallRecords = []
   }
   //紀錄對戰資料
   saveBattle() {
-    // localStorage.setItem('record', JSON.stringify(this.allRecords))
-
-
-
     localStorage.setItem('record', JSON.stringify(this.testallRecords))
   }
   //記錄此次遊戲
   noteGame() {
-    this.nowRecord = []
-    // 處理歷史紀錄
-    let historyTarget:stepType[][] = this.allRecords.slice(1, this.allRecords.length)
-    if(historyTarget.every((item) => item?.length > 0)) {
-      historyTarget.shift()
-      historyTarget.push(this.allRecords[0])
-    }else {
-      for(let i = 0;i<historyTarget.length;i++) {
-        if(!historyTarget[i]?.length) {
-          historyTarget[i] = this.allRecords[0]
-          break
-        }
-      }
-    }
-    // 生成歷史紀錄
-    for(let i = 1;i<=5;i++) this.allRecords[i] = historyTarget[i-1]
-    this.allRecords[0] = []
-    this.saveBattle()
-
-
-
     this.testnowRecord = []
     // 處理歷史紀錄
     let testhistoryTarget:stepType[][][] = this.testallRecords.slice(1, this.testallRecords.length)
